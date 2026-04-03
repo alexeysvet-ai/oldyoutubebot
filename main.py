8c8c70497e35b8289839525d2df04b4f8280260b
alexeysvet-ai <alexeysvet@gmail.com>
2026-04-03 19:08:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from aiohttp import web
 TOKEN = os.getenv("TOKEN")
 BASE_URL = os.getenv("BASE_URL")
 WEBHOOK_PATH = "/webhook"
+PORT = int(os.getenv("PORT", 10000))
 
 if not TOKEN:
     raise ValueError("TOKEN not set")
